28521ebe9b3775b572bee3355af295cd1bbcba09
NishithshettyR <nishith0023@gmail.com>
2021-03-17 00:00:39 +0530

    Coffee

--- public/JAVASCRIPT/cart.js
+++ public/JAVASCRIPT/cart.js
@@ -5,8 +5,8 @@ const item  = document.querySelector(".message");
 
 for(let i = 0;i<remove.length ; i++){
     remove[i].addEventListener("click" , function(){
-        item.classList.add("Deleted");
-        item.textContent = "Deleting...";
+        
+        remove[i].textContent = "Deleting...";
         remove[i].classList.add("movebutton");
         setTimeout(() => {
             remove[i].classList.remove("movebutton");
